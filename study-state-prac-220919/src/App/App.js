a63c66f7c694440c020cc50c66646d4b71a41b0c
yeonjujoeng0511 <yeonjujeong0511@gmail.com>
2022-09-20 10:03:07 +0900

    state로 count 해보기

--- study-state-prac-220919/src/App/App.js
+++ study-state-prac-220919/src/App/App.js
@@ -6,13 +6,17 @@ function App() {
   const increaseNumber = () => {
     setNumber(number + 1);
   };
-  //const decreseNumber = () =>{}
+  const decreseNumber = () => {
+    setNumber(number - 1);
+  };
 
   return (
     <>
       <h1>{number}</h1>
+      <button>추가버튼</button>
       <button onClick={increaseNumber}>+1</button>
-      <button>-1</button>
+      <button onClick={decreseNumber}>-1</button>
+      <div>여기에추가될것</div>
     </>
   );
 }
