00f7169eaaeff0df3534ae242a9453274a570bf8
yeonjujoeng0511 <yeonjujeong0511@gmail.com>
2022-09-21 10:52:13 +0900

    추가 제거 기능 구현

--- study-state-prac-220919/src/App/App.js
+++ study-state-prac-220919/src/App/App.js
@@ -14,10 +14,9 @@ function App() {
   return (
     <>
       <h1>{number}</h1>
-      <button>추가버튼</button>
       <button onClick={increaseNumber}>+1</button>
       <button onClick={decreseNumber}>-1</button>
-      <div>여기에추가될것</div>
+      <br></br>
       <IterationSample></IterationSample>
     </>
   );
