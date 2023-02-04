def394c7ee534d7397a6cd1118b7ec06d6627397
yeonju0511 <yeonjujeong0511@gmail.com>
2023-02-04 23:52:17 +0900

    redux prac

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "./actions";
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLoggend = useSelector(state => state.isLoggend)
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLoggend ? <h3>로그인하세요</h3> : ""}
     </div>
   );
 }
--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -3,48 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
 
+const store = createStore(allReducers)
 
-// STORE -> GLOBAL STATE
-
-// ACTION INCREMENT
-
-const increment = () => {
-  return {
-    type: "INCREMENT"
-  }
-}
-const decrement = () => {
-  return {
-    type: "DECREMENT"
-  }
-}
-
-
-//REDUCER
-
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREAMENT":
-      return state - 1;
-  }
-}
-let store = createStore(counter)
-
-// Display it in the console
-
-
-//DISPATCH
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
