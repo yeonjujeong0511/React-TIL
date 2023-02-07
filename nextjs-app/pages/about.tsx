47888688c602dcd971f0de24f91211ad90affc12
yeonju0511 <yeonjujeong0511@gmail.com>
2023-02-07 14:30:22 +0900

    next.js routing

---
+++ nextjs-app/pages/about.tsx
@@ -0,0 +1,10 @@
+import NavBar from "@/componets/NavBar";
+
+export default function Potato() {
+  return (
+    <div>
+      <NavBar />
+      <h1>About</h1>
+    </div>
+  );
+}
---
+++ nextjs-app/pages/index.tsx
@@ -0,0 +1,10 @@
+import NavBar from "@/componets/NavBar";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Hello</h1>
+    </div>
+  );
+}
